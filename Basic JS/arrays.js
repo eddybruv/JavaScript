a2d50029cb3332f20bbf93dd994fd96f884a4e30
eddybruv <edwinajong@gmail.com>
2021-11-29 20:18:27 +0100

    arrays in JS

--- Basic JS/arrays.js
+++ Basic JS/arrays.js
@@ -1,5 +1,5 @@
 /**
- * @brief basic array
+ * @brief basic arrays
  */
 
 var numbers = [4, 6, 3, 2, 4, 3, 6, 4, 2, 2, 5, 6, 3];
@@ -11,3 +11,6 @@ var arr = [
     [2, 4],
     [41, 3, 2]
 ];
+
+//we can also use the keyword "new"
+var cars = new Array("toyota", "camry", "hummer");
