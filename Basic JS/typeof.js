c81d951566bd764887a86d2c35a8dc94d7adc842
eddybruv <edwinajong@gmail.com>
2021-12-04 14:10:49 +0100

    added comments

--- Basic JS/typeof.js
+++ Basic JS/typeof.js
@@ -2,7 +2,7 @@
  * @brief : Various returns of the type of operator
  */
 
-console.log(typeof(" "));
-console.log(typeof(7));
-console.log(typeof(true));
-console.log(typeof(null));
+console.log(typeof(" ")); //returns string
+console.log(typeof(7)); //returns number
+console.log(typeof(true)); //returns boolean
+console.log(typeof(null)); //returns object
