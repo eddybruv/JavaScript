c64569bf2417d179a30aa563373142a75663be51
Edwin Ajong <edwinajong@gmail.com>
2021-12-08 14:33:32 +0100

    Create dateFormats.js

---
+++ Basic JS/dateFormats.js
@@ -0,0 +1,17 @@
+/**
+  * @brief : JavaScript date formats.
+  */
+
+/**
+  * Date Input:
+  * There are 3 types of JS inout formats:
+  * ISO Date : "2015-10-24"
+  * Short date : "10/24/2015"
+  * Long date : "Oct 24 2015" 
+  */
+
+/**
+  * JavaScript Date Output :
+  * Independent of input format, JavaScript will (by default) output dates in full text string format:
+  * Wed Dec 08 2021 14:21:20 GMT+0100 (West Africa Standard Time)
+  */
