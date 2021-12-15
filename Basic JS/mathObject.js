8f64b631d62da049adab6be4cc9b3821c34df6d8
eddybruv <edwinajong@gmail.com>
2021-12-15 20:50:46 +0100

    some few math properties

---
+++ Basic JS/mathObject.js
@@ -0,0 +1,9 @@
+/**
+ * The math object has no constructor, it is static and all methods
+ * and properties can be used without creating a math object first.
+ */
+
+//Math Properties
+console.log(Math.PI) //returns PI
+console.log(Math.E) //returns Eulers Number
+console.log(Math.SQRT2) //returns the square root of 2
