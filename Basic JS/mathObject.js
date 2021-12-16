373a5e2891fc07a6d19ca5d906605f8831b06c59
eddybruv <edwinajong@gmail.com>
2021-12-16 19:51:11 +0100

    finished adding math properties and methods

--- Basic JS/mathObject.js
+++ Basic JS/mathObject.js
@@ -1,9 +1,31 @@
 /**
  * The math object has no constructor, it is static and all methods
  * and properties can be used without creating a math object first.
+ * Math properties are constants
  */
 
 //Math Properties
 console.log(Math.PI) //returns PI
 console.log(Math.E) //returns Euler's Number
 console.log(Math.SQRT2) //returns the square root of 2
+console.log(Math.LN2) //returns the natural logarithm of 2
+
+//Math Methods
+Math.round(78.3); //returns 78
+Math.round(89.7); //returns 90
+
+/**
+ * Math.floor(x): returns the value of x rounded down to its nearest integer
+ * Math.ceil(x):  returns the value of x rounded up to its nearest integer 
+ * Math.truck(x): returns the integer part of x.
+ * Math.sign(x):  returns if x is -ve, +ve of null
+ * Math.pow(x, y): returns the value of x to the power y.
+ * Math.sqrt(x): returns the square root of x.
+ * Math.abs(x): returns the positive value of x.
+ * Math.sin(x): returns the sine of the angle x(given in radians).
+ * Math.cos(x): returns the cosine of the angle x.
+ * Math.min(), Math.max(): can be used to find the lowest or highest value in a list of arguments:
+ * Math.min(3, 2, 3,2, 3232, 32, 1 2, 0);   ==> returns 0
+ * Math.max(3, 2, 3,2, 3232, 32, 1 2, 0);   ==> returns 3232
+ * Math.random(): returns a random number between 0 (inclusive), and 1 (exclusive)
+ */
