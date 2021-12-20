87b5d5604463643c5226b3c58a8743c22949064d
eddybruv <edwinajong@gmail.com>
2021-12-20 11:54:51 +0100

    more objects😅

---
+++ FCC/Notes/nestedObj.js
@@ -0,0 +1,20 @@
+/**
+ * @brief : Accessing Nested Objects
+ */
+
+//sub-properties can be accessed by chaining together the dot or bracket notation
+const ourStorage = {
+  "desk": {
+    "drawer": "stapler"
+  },
+  "cabinet": {
+    "top drawer": { 
+      "folder1": "a file",
+      "folder2": "secrets"
+    },
+    "bottom drawer": "soda"
+  }
+};
+
+ourStorage.cabinet["top drawer"].folder2;
+ourStorage.desk.drawer;
