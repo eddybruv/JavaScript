cdb96c557b0c73cec5ca1c31d94a0c0667282c88
eddybruv <edwinajong@gmail.com>
2021-12-22 13:58:04 +0100

    description in file

---
+++ FCC/ES6/Notes/des-assignment2.js
@@ -0,0 +1,8 @@
+/**
+ * @brief : Use destructuring assignment to assign variables from arrays
+ */
+
+const [a, b] = [1, 2, 3, 4, 5, 6, 7, 8,9];
+console.log(a, b);
+const [a, b, , , , , , c] = [1, 2, 3, 4, 5, 6, 7, 8,9];
+console.log(a, b, c);
