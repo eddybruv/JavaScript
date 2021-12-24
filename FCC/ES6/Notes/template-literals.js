41b5665203b11b6629268ba0e53edf4bec7062ae
eddybruv <edwinajong@gmail.com>
2021-12-24 18:43:01 +0100

    template literals

---
+++ FCC/ES6/Notes/template-literals.js
@@ -0,0 +1,11 @@
+/**
+ * @brief : Create strings using template literals
+ */
+
+const person = {
+    name: "John Doe",
+    age: 22
+};
+
+const greeting = `Hello, my name is ${person.name}
+I am ${person.age} years old`;
