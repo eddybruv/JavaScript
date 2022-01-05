43e91bb2dc681a362bbdb6a42f2b01d993a76a8e
eddybruv <edwinajong@gmail.com>
2022-01-05 06:16:30 +0100

    Create a javaScript promise

---
+++ FCC/ES6/Notes/promise.js
@@ -0,0 +1,26 @@
+/**
+ * @brief : Create a javaScript promise
+ * A promise in JavaScript is exactly what it sounds like - you use it to make a promise to do something, 
+ * usually asynchronously. 
+ * When the task completes, you either fulfill your promise or fail to do so. 
+ * Promise is a constructor function, so you need to use the new keyword to create one. 
+ * It takes a function, as its argument, with two parameters - resolve and reject. 
+ * These are methods used to determine the outcome of the promise. 
+ * The syntax looks like this:
+ */
+
+const myPromise = new Promise((resolve, reject) => {
+    //forever in the pending state
+});
+
+/**
+  
+    const myPromise = new Promise((resolve, reject) => {
+    if(condition here) {
+        resolve("Promise was fulfilled");
+    } else {
+        reject("Promise was rejected");
+    }
+    });
+
+*/
