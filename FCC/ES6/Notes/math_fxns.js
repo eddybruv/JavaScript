9f96a8864324ea17da4761f6c00ac29af17534c4
eddybruv <edwinajong@gmail.com>
2022-01-05 06:15:07 +0100

    Use export to share a code block

---
+++ FCC/ES6/Notes/math_fxns.js
@@ -0,0 +1,13 @@
+/**
+ * @brief : Use export to share a code block
+ */
+
+const add = (x, y) => {
+    return x + y;
+}
+
+const substract = (x, y) => {
+    return x - y;
+}
+
+export {add, substract};
