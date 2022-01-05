874e8f647dd781821fa631dbdf5fa4a86d64ace9
eddybruv <edwinajong@gmail.com>
2022-01-05 06:14:27 +0100

    Reuse JS code using import

---
+++ FCC/ES6/Notes/import_math.js
@@ -0,0 +1,9 @@
+import { add, substract } from './math_fxns.js'
+/**
+ * @brief : Reuse JS code using import
+ * 'math_fxns.js' is the JS code to be imported
+ */
+
+
+console.log(add(3, 6));
+console.log(substract(89, 3));
