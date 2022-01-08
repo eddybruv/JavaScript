89f2b4bfa151fdbfa65e6a5bff56abb16eefe103
eddybruv <edwinajong@gmail.com>
2022-01-08 13:56:04 +0100

    match chareacters that occur zero or many times

---
+++ FCC/Regular Expressions/Notes/zero-more.js
@@ -0,0 +1,12 @@
+/**
+ * @brief : There's an option that matches characters that occur zero or more times.
+ * We use the * character.
+ */
+
+let soccerWord = "gooooooooal!";
+let gPhrase = "gut feeling";
+let oPhrase = "over the moon";
+let goRegex = /go*/;
+soccerWord.match(goRegex);
+gPhrase.match(goRegex);
+oPhrase.match(goRegex);
