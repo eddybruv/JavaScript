4b819ae0ed059c0d5c67a3f69b5cef17804c550d
eddybruv <edwinajong@gmail.com>
2022-01-24 13:18:07 +0100

    Find characters with Greedy matching

---
+++ FCC/Regular Expressions/Notes/greedy-match.js
@@ -0,0 +1,8 @@
+/**
+ * A greedy match finds the longest possible part of a 
+ * string that fits the regex pattern and returns it as a
+ * match.
+ * 
+ * eg
+ * /t[a-z]*i/ to the string "titanic" would return ["titani"]
+ */
