9979dd59466c0539966342ee60ea36bc234d7aa3
eddybruv <edwinajong@gmail.com>
2022-01-27 20:05:49 +0100

    Remove items using splice()

---
+++ FCC/Basic Data Structures/Notes/d-splice.js
@@ -0,0 +1,15 @@
+/**
+ * splice() --> remove any number of consecutive elements from anywhere in an array
+ * returns an array of elements removed
+ */
+
+let array = ["today", "was", "not", "so", "great"];
+array.splice(2, 2);
+/**
+ * args:
+ * first --> starting index
+ * second --> number of elements to remove
+ * 
+ * using just one argument removes every element including that element till the end of the list/array
+ */
+console.log(array);
