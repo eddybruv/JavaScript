260fac19c3b6a3f21b16442e13ca87bcd709ccf6
eddybruv <edwinajong@gmail.com>
2022-01-27 20:03:30 +0100

    Add key-value pairs to javascript objects

---
+++ FCC/Basic Data Structures/Notes/key-value-pairs.js
@@ -0,0 +1,14 @@
+/**
+ * Objects are collections of key-value pairs.
+ * They are pieces of data mapped to unique identifiers called keys
+ */
+
+const tekkenCharacter = {
+    player: 'GI Joe',
+    fightingStyle: 'Tae Kwon Doe',
+    human: true
+};
+
+//we can add properties to this character using bracket notation or by assigning
+tekkenCharacter['hair type'] = "black curvy";   //bracket notation
+tekkenCharacter.origin = "South Korea";         //assigning
