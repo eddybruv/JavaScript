aa2e2786096017bdb48a38e594de3f48bc55de82
eddybruv <edwinajong@gmail.com>
2022-01-27 20:04:26 +0100

    Check for the presence of an element using indexOf()

---
+++ FCC/Basic Data Structures/Notes/indexof.js
@@ -0,0 +1,10 @@
+/**
+ * indexOf()    --> takes an element as a parameter and returns the position or index of that element
+ *                  it returns -1 if the element was not found
+ */
+
+let fruits = ["apples", "pears", "oranges", "peaches", "pears"];
+
+fruits.indexOf("dates");
+fruits.indexOf("oranges");
+fruits.indexOf("pears");
