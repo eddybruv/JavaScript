427434670ad817a9e4ec8baf0b0a5657d8b2ce3b
eddybruv <edwinajong@gmail.com>
2022-01-27 20:00:42 +0100

    Remove Items from an array using pop() and shift()

---
+++ FCC/Basic Data Structures/Notes/pop-shift.js
@@ -0,0 +1,9 @@
+/**
+ * pop()    --> removes an element from the end of an array
+ * shift()  --> removes an element from the beginning
+ */
+
+let greetings = ["what's up", "hello", "see ya"];
+
+console.log(greetings.pop()); //returns 'see ya'
+console.log(greetings.shift()); //returns 'what's up?'
