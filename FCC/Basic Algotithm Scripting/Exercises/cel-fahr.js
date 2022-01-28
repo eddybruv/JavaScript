392fb1c31eca31be8725e349e96bb4bd09cb4c38
eddybruv <edwinajong@gmail.com>
2022-01-28 21:45:00 +0100

    Convert celsius to Fahrenheit

---
+++ FCC/Basic Algotithm Scripting/Exercises/cel-fahr.js
@@ -0,0 +1,10 @@
+/**
+ * Write a funtion that converts temp from cel to fahr
+ */
+
+function convertToF(celsius) {
+  let fahrenheit = (celsius * 9) / 5 + 32;
+  return fahrenheit;
+}
+
+convertToF(30);
