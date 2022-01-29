dc8dfd68c9f59edaa6806b917a3981c0f01176db
eddybruv <edwinajong@gmail.com>
2022-01-29 21:18:54 +0100

    Repeat a string repeat a string

---
+++ FCC/Basic Algotithm Scripting/FCC solution/repeat.js
@@ -0,0 +1,11 @@
+/**
+ * Return a string containing a repeated string n(num) times
+ */
+
+function repeatStringNumTimes(str, num) {
+  if (num < 1) {
+    return "";
+  } else {
+    return str + repeatStringNumTimes(str, num - 1);
+  }
+}
