5e0127106a3595dd0dbcfa64a0745650d9ecda29
eddybruv <edwinajong@gmail.com>
2022-02-02 21:48:37 +0100

    Create a basic JS object

---
+++ FCC/Object Oriented Programming/Notes/basic-obj.js
@@ -0,0 +1,13 @@
+/**
+ * Create a dog object with name and numLegs property, set them to
+ * a string and number, respectively.
+ * And access the properties using dot notation
+ */
+
+let dog = {
+    name: "senior",
+    numLegs: 4,
+};
+
+const str = `The of the dog is ${dog.name} and has ${dog.numLegs} legs`;
+console.log(str);
