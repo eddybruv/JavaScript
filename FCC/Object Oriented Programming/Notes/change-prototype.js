bac0e2ac443b358051640e2f4a6ded6cf3503797
eddybruv <edwinajong@gmail.com>
2022-02-14 05:27:21 +0100

    Change prototype to new object

---
+++ FCC/Object Oriented Programming/Notes/change-prototype.js
@@ -0,0 +1,19 @@
+/**
+ * Setting the prototype to a new object that contains properties.
+ * This way all the properties are added at once.
+ */
+
+function Dog(name) {
+  this.name = name;
+}
+
+Dog.prototype = {
+  // Only change code below this line
+  numLegs: 4,
+  eat: function () {
+    console.log("nom nom nom");
+  },
+  describe: function () {
+    console.log("My name is " + this.name);
+  },
+};
