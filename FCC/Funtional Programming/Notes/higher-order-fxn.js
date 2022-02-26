38a94219adc4ad0c9e264f1aaea9cd772a972224
eddybruv <edwinajong@gmail.com>
2022-02-26 05:29:35 +0100

    use higher order fn map, filter or reduce

---
+++ FCC/Funtional Programming/Notes/higher-order-fxn.js
@@ -0,0 +1,13 @@
+// Complete the code for the squareList function using any combination of map(), filter(), and reduce(). The function should return a new array containing the squares of only the positive integers (decimal numbers are not integers) when an array of real numbers is passed to it. An example of an array of real numbers is [-3, 4.8, 5, 3, -3.2].
+
+const squareList = (arr) => {
+  // Only change code below this line
+  arr = arr
+    .filter((num) => num > 0 && num - parseInt(num) === 0)
+    .map((num) => num * num);
+  return arr;
+  // Only change code above this line
+};
+
+const squaredIntegers = squareList([-3, 4.8, 5, 3, -3.2]);
+console.log(squaredIntegers);
