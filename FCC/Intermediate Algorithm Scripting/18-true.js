fbdc738d9eaa702f8778e2bb0191047c87a18bff
eddybruv <edwinajong@gmail.com>
2022-02-28 18:20:04 +0100

    Everything be true

---
+++ FCC/Intermediate Algorithm Scripting/18-true.js
@@ -0,0 +1,30 @@
+// Check if the predicate (second argument) is truthy on all elements of a collection (first argument).
+
+// In other words, you are given an array collection of objects. 
+//The predicate pre will be an object property and you need to return true if its value is truthy. Otherwise, return false.
+
+// In JavaScript, truthy values are values that translate to true when evaluated in a Boolean context.
+
+// Remember, you can access object properties through either dot notation or [] notation.
+
+
+function truthCheck(collection, pre) {
+  let counter = 0;
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i].hasOwnProperty(pre) && Boolean(collection[i][pre]))
+      counter++;
+  }
+  return counter++ == collection.length;
+}
+
+console.log(
+  truthCheck(
+    [
+      { user: "Tinky-Winky", sex: "male" },
+      { user: "Dipsy" },
+      { user: "Laa-Laa", sex: "female" },
+      { user: "Po", sex: "female" },
+    ],
+    "sex"
+  )
+);
