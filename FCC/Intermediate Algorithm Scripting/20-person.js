9526e0a21adfa810f64ad149849ff9712be21d05
eddybruv <edwinajong@gmail.com>
2022-02-28 18:20:26 +0100

    make a person

---
+++ FCC/Intermediate Algorithm Scripting/20-person.js
@@ -0,0 +1,44 @@
+// Fill in the object constructor with the following methods below:
+
+// getFirstName()
+// getLastName()
+// getFullName()
+// setFirstName(first)
+// setLastName(last)
+// setFullName(firstAndLast)
+
+// Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+
+
+const Person = function (firstAndLast) {
+  // Only change code below this line
+  // Complete the method below and implement the others similarly
+  let fullName = firstAndLast;
+
+  this.getFirstName = function () {
+    return fullName.split(" ")[0];
+  };
+
+  this.getLastName = function () {
+    return fullName.split(" ")[1];
+  };
+
+  this.getFullName = function () {
+    return fullName;
+  };
+
+  this.setFirstName = function (first) {
+    fullName = first + " " + fullName.split(" ")[1];
+  };
+
+  this.setLastName = function (last) {
+    fullName = fullName.split(" ")[0] + " " + last;
+  };
+
+  this.setFullName = function (name) {
+    fullName = name;
+  };
+};
+
+const bob = new Person("Bob Ross");
+console.log(bob.setFirstName("cur"));
